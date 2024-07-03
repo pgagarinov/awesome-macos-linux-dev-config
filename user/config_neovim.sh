655efe5b80824d4f876f3b22106e0ff31af4aa35
Peter <peter@Peters-Virtual-Machine.local>
2024-07-03 14:13:57 +0200

    Migrated to LazyVim

--- user/config_neovim.sh
+++ user/config_neovim.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-rm -rf ~/.config/nvim
+BAK_EXTENSION=.bak.$(date +%Y%m%d%H%M%S)
+mv ~/.config/nvim{,$BAK_EXTENSION} || true
+mv ~/.local/share/nvim{,$BAK_EXTENSION} || true
+mv ~/.local/state/nvim{,$BAK_EXTENSION} || true
+mv ~/.cache/nvim{,$BAK_EXTENSION} || true
+
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 cp -r $dir_path/config/.config/nvim ~/.config
-nvim --headless  +PlugInstall +qa
-nvim --headless  +"CocInstall coc-rls" +qa
